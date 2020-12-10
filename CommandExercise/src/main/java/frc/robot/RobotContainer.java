548eefe1153e93a79e4c3798551a6e29838f4222
Sanjana <sanjana.la36@gmail.comgit config --global user.email sanjana.la36@gmail.comgit config --global user.email sanjana.la36@gmail.com>
2020-12-10 18:50:04 -0500

    command

--- CommandExercise/src/main/java/frc/robot/RobotContainer.java
+++ CommandExercise/src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SanjanaCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SanjanaSubsystem;
 import frc.robot.subsystems.SravaniSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -31,6 +33,8 @@ public class RobotContainer {
   private final SravaniSubsystem m_sravaniSubsystem = new SravaniSubsystem();
   private final SravaniCommand m_sravaniCommand = new SravaniCommand(m_sravaniSubsystem);
 
+  private final SanjanaSubsystem m_sanjana = new SanjanaSubsystem();
+  private final SanjanaCommand m_sanjanaCommand = new SanjanaCommand(m_sanjana);
 
 
   /**
@@ -40,6 +44,7 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     CommandScheduler.getInstance().setDefaultCommand(m_sravaniSubsystem, m_sravaniCommand);
+    CommandScheduler.getInstance().setDefaultCommand(m_sanjana, m_sanjanaCommand);
   }
 
   /**
